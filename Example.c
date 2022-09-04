7f68c2d654fc27c2dc9d3774107257b606628161
Burak Çağrı DUBA <201200002@ogr.cbu.edu.tr>
2022-09-05 00:27:19 +0300

    Add project files.

---
+++ Example.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+/*
+	toupper fonksiyonu küçük bir karakteri büyük harfe,
+	tolower fonksiyonu da büyük harf bir karakteri küçük harfe dönüştürür.
+	int toupper(int ch);
+	int tolower(int ch);
+*/
+
+
+int main(void)
+{
+	int ch;
+
+	printf("(e)vet/(h)ayir?\n");
+	ch = getchar();
+	ch = tolower(ch);
+
+	if (ch == 'e')
+		printf("evet\n");
+	else if (ch == 'h')
+		printf("hayir\n");
+	else
+		printf("e ya da h girilmedi!\n");
+
+
+	return 0;
+}
